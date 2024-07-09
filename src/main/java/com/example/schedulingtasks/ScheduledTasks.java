97497133b9adba698eac9d7c1f0ea1292e6a9b43
LetgoMoveNotLate <hj981203@gmail.com>
2024-07-09 15:05:28 +0900

    spring guide 작성

--- src/main/java/com/example/schedulingtasks/ScheduledTasks.java
+++ src/main/java/com/example/schedulingtasks/ScheduledTasks.java
@@ -1,2 +1,24 @@
-package com.example.schedulingtasks;public class ScheduledTasks {
+package com.example.schedulingtasks;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dataFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dataFormat.format(new Date()));
+    }
+
 }
